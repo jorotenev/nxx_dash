491eb246e72ebbf633938dfcc495ac7cbaf66969
Georgi Tenev <georgetenevbg@gmail.com>
2019-02-13 23:57:05 +0200

    Extract ssh to a module

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,9 @@
+mod modules;
+use self::modules::ssh;
 
-use std::net::TcpStream;
-use ssh2::Session;
-use std::path::Path;
-use std::io::prelude::*;
 
 fn main() {
-    let tcp = TcpStream::connect("18.185.96.63:22").unwrap();
-    let mut sess = Session::new().unwrap();
-    sess.handshake(&tcp).unwrap();
-    sess.userauth_pubkey_file("ubuntu", None, Path::new("C:\\Users\\Joro\\Downloads\\jorotenev-fr-2019.pem"), None).unwrap();
-    let mut channel = sess.channel_session().unwrap();
-    channel.exec("ls -a").unwrap();
-    let mut s = String::new();
-    channel.read_to_string(&mut s).unwrap();
-    println!("{}", s);
-    channel.wait_close().unwrap();
-    println!("{}", channel.exit_status().unwrap());
-
-
+    let command = "ls -a";
+    let ip = "18.185.96.63:22";
+    ssh::invoke_command(command, ip)
 }
- 
---
+++ src/ssh.rs
@@ -0,0 +1,26 @@
+pub mod ssh {
+    use ssh2::Session;
+    use std::io::prelude::*;
+    use std::net::TcpStream;
+    use std::path::Path;
+
+    pub fn invoke_command(command: &str, ip: String) {
+        let tcp = TcpStream::connect(ip).unwrap();
+        let mut sess = Session::new().unwrap();
+        sess.handshake(&tcp).unwrap();
+        sess.userauth_pubkey_file(
+            "ubuntu",
+            None,
+            Path::new("C:\\Users\\Joro\\Downloads\\jorotenev-fr-2019.pem"),
+            None,
+        )
+        .unwrap();
+        let mut channel = sess.channel_session().unwrap();
+        channel.exec(command).unwrap();
+        let mut s = String::new();
+        channel.read_to_string(&mut s).unwrap();
+        println!("{}", s);
+        channel.wait_close().unwrap();
+        println!("{}", channel.exit_status().unwrap());
+    }
+}
